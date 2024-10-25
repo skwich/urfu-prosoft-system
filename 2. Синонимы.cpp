b8173a529bcac1719578f18d7664086d4831a50b
skwich <a.3066@mail.ru>
2024-10-26 00:52:10 +0500

    WA test 9 (253ms / 6.06Mb)

--- 2. Синонимы.cpp
+++ 2. Синонимы.cpp
@@ -1,44 +1,9 @@
 #include <unordered_map>
+#include <algorithm>
 #include <iostream>
 #include <string>
 
 std::unordered_map<std::string, std::string> map;
-
-void execute_operation(const std::string& operation)
-{
-    std::string param1;
-    std::cin >> param1;
-
-    if (operation == "ADD")
-    {
-        std::string param2;
-        std::cin >> param2;
-
-        map[param1] += param2 + ',';
-        map[param2] += param1 + ',';
-    }
-    else if (operation == "COUNT")
-    {
-        size_t counter = 0;
-        size_t finder = map[param1].find(',');
-        while (finder != std::string::npos)
-        {
-            ++counter;
-            finder = map[param1].find(',', finder+1);
-        }
-        std::cout << counter << std::endl;
-    }
-    else if (operation == "CHECK")
-    {
-        std::string param2;
-        std::cin >> param2;
-
-        if (map[param1].find(param2) != std::string::npos)
-            std::cout << "YES" << std::endl;
-        else
-            std::cout << "NO" << std::endl;
-    }
-}
 
 int main()
 {
@@ -49,6 +14,30 @@ int main()
         std::string operation;
         std::cin >> operation;
 
-        execute_operation(operation);
+        std::string param1;
+        std::cin >> param1;
+
+        if (operation == "ADD")
+        {
+            std::string param2;
+            std::cin >> param2;
+
+            map[param1] += param2 + ',';
+            map[param2] += param1 + ',';
+        }
+        else if (operation == "COUNT")
+        {
+            std::cout << std::count(map[param1].begin(), map[param1].end(), ',') << std::endl;
+        }
+        else if (operation == "CHECK")
+        {
+            std::string param2;
+            std::cin >> param2;
+
+            if (map[param1].find(param2) != std::string::npos)
+                std::cout << "YES" << std::endl;
+            else
+                std::cout << "NO" << std::endl;
+        }
     }
 }
--- test.cpp
+++ test.cpp
@@ -1,52 +1,22 @@
 #include <unordered_map>
-#include <vector>
 #include <iostream>
 #include <string>
 
-void print_vector(std::vector<std::string> vec)
-{
-    for (const auto& it : vec)
-    {
-        std::cout << it << ", ";
-    }
-}
-
-void print_map(const std::unordered_map<std::string, std::vector<std::string>>& map)
-{
-    for (const auto& [key, value] : map)
-    {
-        std::cout << "first: " << key
-                  << " second: ";
-        print_vector(value);
-        std::cout << std::endl;
-    }
-}
-
-void print_map(const std::unordered_map<std::string, std::string>& map)
-{
-    for (const auto& it : map)
-    {
-        std::cout << "first: " << it.first
-                  << " second: " << it.second
-                  << std::endl;
-    }
-}
-
 int main()
 {
-    // std::unordered_map<std::string, std::vector<std::string>> map;
-    // map["code"].push_back("cpp");
-    // map["code"].push_back("java");
-    // map["code"].push_back("python");
+    std::unordered_multimap<std::string, std::string> map = {
+        {"1", "1"},
+        {"1", "2"},
+        {"1", "3"},
+        {"2", "1"},
+    };
 
-    std::unordered_map<std::string, std::string> map;
-    // std::cout << map["code"].size() << std::endl;
-    map["program"] += "code,";
-    map["program"] += "ded,";
-    std::cout << map["program"] << std::endl;
-    // std::cout << map["program"].append("dad") << std::endl;
-    // map["program"].append(",dad");
-    // map["program"].append(",dad");
-    // std::cout << map.at("program") << std::endl;
-    print_map(map);
+    auto range = map.equal_range("2");
+    for (auto it = range.first; it != range.second; ++it)
+    {
+        if (it->second == "1")
+        {
+            std::cout << "found!" << std::endl;
+        }
+    }
 }
