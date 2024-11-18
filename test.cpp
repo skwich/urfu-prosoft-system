043728232fccac13f9c9e40eb64e7624e8b8d890
skwich <a.3066@mail.ru>
2024-11-18 17:05:39 +0500

    COMPLETE TASK

--- test.cpp
+++ test.cpp
@@ -4,19 +4,8 @@
 
 int main()
 {
-    std::unordered_multimap<std::string, std::string> map = {
-        {"1", "1"},
-        {"1", "2"},
-        {"1", "3"},
-        {"2", "1"},
-    };
+    std::string s1;
+    std::cin >> s1;
 
-    auto range = map.equal_range("2");
-    for (auto it = range.first; it != range.second; ++it)
-    {
-        if (it->second == "1")
-        {
-            std::cout << "found!" << std::endl;
-        }
-    }
+    std::cout << s1 << std::endl;
 }
